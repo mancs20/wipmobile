a062bf769a56030e686608609292b02d8deb5bbf
mancs20 <combarro87@gmail.com>
2018-02-21 23:49:08 -0800

    commit test

---
+++ app/src/main/java/com/products/qc/CamerasSettingActivity.java
@@ -0,0 +1,34 @@
+package com.products.qc;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+public class CamerasSettingActivity extends ActionBarActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setContentView(R.layout.my_activity);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter();
+
+        // specify an adapter (see also next example)
+        mAdapter = new CamerasIPAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);*/
+    }
+}
