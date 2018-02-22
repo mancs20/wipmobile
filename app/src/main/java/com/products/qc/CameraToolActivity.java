15e442b2e1584e67aa8f6c7c39ff8ea104c70386
mancs20 <combarro87@gmail.com>
2018-02-21 17:33:13 -0800

    settings begining

--- app/src/main/java/com/products/qc/CameraToolActivity.java
+++ app/src/main/java/com/products/qc/CameraToolActivity.java
@@ -2,6 +2,10 @@ package com.products.qc;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class CameraToolActivity extends ActionBarActivity {
 
@@ -9,5 +13,16 @@ public class CameraToolActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_tool);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.camera_tool_menu, menu);
+        return true;
+    }
+
+    public void showCameraIPSettings(MenuItem item){
+        Toast.makeText(getApplicationContext(), "funcino", Toast.LENGTH_LONG).show();
     }
 }
